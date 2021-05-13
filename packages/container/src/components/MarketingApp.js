ac32b94a9bb574f5b1c7a379f559f26bd74e0f29
yd1722 <y.dominatz@gmail.com>
2021-05-14 00:24:59 +0530

    BUG | Micro Front End - Dashboard | fix import issues

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from "react";
-import { mount } from "marketing/marketingApp";
+import { mount } from "marketing/MarketingApp";
 
 export default () => {
   const ref = useRef(null);
