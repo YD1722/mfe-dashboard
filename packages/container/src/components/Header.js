f8c50e09f0fbdef4a11447b1983c06fb6d81502f
yd1722 <y.dominatz@gmail.com>
2021-05-16 13:18:20 +0530

    INT | Micro Front End - Dashboard | Trigger workflow

--- packages/container/src/components/Header.js
+++ packages/container/src/components/Header.js
@@ -79,7 +79,7 @@ export default function Header({ signedIn, onSignOut }) {
             component={RouterLink}
             to="/"
           >
-            MFE
+            MFE -demo
           </Typography>
           <Button
             color="primary"
