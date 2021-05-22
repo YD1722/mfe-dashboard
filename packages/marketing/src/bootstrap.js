5bd2fd6b6021f81c6981ba9b43c644c39f9191c1
yd1722 <y.dominatz@gmail.com>
2021-05-22 19:45:27 +0530

    INT | Micro Front End - Dashboard | Router Implementation

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -3,17 +3,25 @@ import ReactDOM from "react-dom";
 import { createMemoryHistory } from "history";
 import App from "./App";
 
-const mount = (el) => {
+const mount = (el, { onNavigate }) => {
   const history = createMemoryHistory();
 
+  history.listen(onNavigate);
+
   ReactDOM.render(<App history={history} />, el);
+
+  return {
+    onParentNavigate() {
+      console.log("Container just navigated");
+    },
+  };
 };
 
 if (process.env.NODE_ENV === "development") {
   const devRoot = document.querySelector("#_marketing-dev-root");
 
   if (devRoot) {
-    mount(devRoot);
+    mount(devRoot, {});
   }
 }
 
